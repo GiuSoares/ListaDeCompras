e08c0cc174e3c0520e5e74ceb89163313bb59d7c
Giulia Soares Rego dos Santos <giulias@dbserver.com.br>
2019-04-19 23:46:09 -0300

    Adicionada evento no botao

---
+++ app/src/main/java/com/giulias/listadecompras/AddListaActivity.java
@@ -0,0 +1,14 @@
+package com.giulias.listadecompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lista);
+
+    }
+}
--- app/src/main/java/com/giulias/listadecompras/MainActivity.java
+++ app/src/main/java/com/giulias/listadecompras/MainActivity.java
@@ -1,13 +1,37 @@
 package com.giulias.listadecompras;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder= new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mViewHolder.addListaButton= (Button)findViewById(R.id.button_add_lista);
+        this.mViewHolder.addListaButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id= view.getId();
+        if (id==R.id.button_add_lista){
+            //this.mViewHolder.addListaButton.setText("Bombou!");
+        }
+    }
+
+
+    private static class ViewHolder{
+
+        Button addListaButton;
+
+
     }
 }
